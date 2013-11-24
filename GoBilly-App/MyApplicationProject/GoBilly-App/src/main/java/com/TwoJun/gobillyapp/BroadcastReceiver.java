a7a6f577a615adc6c21fbda9ecea23fc23de42da
Arjun Shanker <rjnshanker@gmail.com>
2013-11-24 02:01:44 -0600

    Added basic widget stuff

---
+++ GoBilly-App/MyApplicationProject/GoBilly-App/src/main/java/com/TwoJun/gobillyapp/BroadcastReceiver.java
@@ -0,0 +1,37 @@
+package com.TwoJun.gobillyapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Arjun on 11/24/13.
+ */
+public class BroadcastReceiver extends AppWidgetProvider{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+}
+
